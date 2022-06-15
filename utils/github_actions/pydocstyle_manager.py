521ebbaa9f24c4efa2d124bc82671a531915215d
Valentin Goldité <68942619+valentingol@users.noreply.github.com>
2022-06-15 14:32:09 +0200

    :white_check_mark: Add pytest failure catching
    * ✅ Add test error check
    
    * ♻️ Refactoring and improve names of variables
    
    * 🥅 Catch Pytest failures
    
    * :memo:  Better job name

--- utils/github_actions/pydocstyle_manager.py
+++ utils/github_actions/pydocstyle_manager.py
@@ -2,8 +2,23 @@
 import sys
 
 
-if __name__ == '__main__':
-    score = int((sys.argv[1]).split('=')[1])
-    if score > 0:
-        raise ValueError(f'Pydocstyle found {score} errors in python '
+def check_output() -> None:
+    """Check output of Pydocstyle.
+
+    Raises
+    ------
+    ValueError
+        If Pydocstyle find errors.
+    """
+    args = sys.argv[1:]
+    for arg in args:
+        if arg.startswith('--n_errors='):
+            n_errors = int(arg.split('=')[1])
+
+    if n_errors > 0:
+        raise ValueError(f'Pydocstyle found {n_errors} errors in python '
                          'docstrings. Please fix them.')
+
+
+if __name__ == '__main__':
+    check_output()
