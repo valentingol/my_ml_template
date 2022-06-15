0869aef6a81d5ede59c4317295bd21493b09ab0e
Valentin Goldité <valentin.goldite@gmail.com>
2022-06-15 13:05:47 +0200

    🏷️ Add type hints to follow mypy

--- src/some_code.py
+++ src/some_code.py
@@ -1,7 +1,7 @@
 """Some code to be checked by linters, pytests and so on."""
 
 
-def add_integers(arg1, arg2):
+def add_integers(arg1: int, arg2: int) -> int:
     """Add two integers."""
     if not isinstance(arg1, int) or not isinstance(arg2, int):
         raise TypeError('arg1 and arg2 must be integers')
--- utils/github_actions/color.py
+++ utils/github_actions/color.py
@@ -2,11 +2,12 @@
 from colorsys import hsv_to_rgb
 
 
-def score_to_hex_color(score, score_min, score_max):
+def score_to_hex_color(score: float, score_min: float,
+                       score_max: float) -> str:
     """Convert score to hex color red > brightgreen."""
     norm_score = max(0, (score - score_min) / (score_max - score_min))
     hsv = (1/3 * norm_score, 1, 1)
     rgb = hsv_to_rgb(*hsv)
-    rgb = tuple(int(255 * value) for value in rgb)
-    hex_color = f'#{rgb[0]:02x}{rgb[1]:02x}{rgb[2]:02x}'
+    rgb_tuple = tuple(int(255 * value) for value in rgb)
+    hex_color = f'#{rgb_tuple[0]:02x}{rgb_tuple[1]:02x}{rgb_tuple[2]:02x}'
     return hex_color
