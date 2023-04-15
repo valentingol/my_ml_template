83ed3101d760b7d691d805cb4ce0e9645926c13d
Valentin Goldité <valentin.goldite@gmail.com>
2023-04-15 17:05:12 +0200

    🚚 Rename src folder to template

--- template/some_code.py
+++ template/some_code.py
@@ -1,8 +1,8 @@
-"""Some code to be checked by linters, pytests and so on."""
+"""Some code to be checked by linters, tests and so on."""
 
 
 def add_integers(arg1: int, arg2: int) -> int:
     """Add two integers."""
     if not isinstance(arg1, int) or not isinstance(arg2, int):
-        raise TypeError('arg1 and arg2 must be integers')
+        raise TypeError("arg1 and arg2 must be integers")
     return arg1 + arg2
