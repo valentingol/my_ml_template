7dec75d68560ba660b72d311918a99cd16e66991
Valentin Goldité <valentin.goldite@gmail.com>
2023-04-22 19:29:10 +0200

    🎨 Improve style (black)

--- tests/test_main.py
+++ tests/test_main.py
@@ -13,20 +13,23 @@ def test_main(mocker: MockerFixture) -> None:
     mocker.patch("template.inference.infer.run")
     mocker.patch("template.training.train.run")
     # training mode
-    run = os.system("python template/main.py --mode='train' "
-                    "--experiment_path='tests/tmp/1'")
+    run = os.system(
+        "python template/main.py --mode='train' --experiment_path='tests/tmp/1'"
+    )
     check.equal(run, 0)
     check.is_true(os.path.exists("tests/tmp/1_0/config.yaml"))
     check.is_true(os.path.exists("tests/tmp/1_0/config_hierarchy.yaml"))
     # inference mode
-    run = os.system("python template/main.py --mode='infer' "
-                    "--experiment_path='tests/tmp/2'")
+    run = os.system(
+        "python template/main.py --mode='infer' --experiment_path='tests/tmp/2'"
+    )
     check.equal(run, 0)
     check.is_true(os.path.exists("tests/tmp/1_0/config.yaml"))
     check.is_true(os.path.exists("tests/tmp/1_0/config_hierarchy.yaml"))
     # unknown mode
-    run = os.system("python template/main.py --mode='UNKNOWN' "
-                    "--experiment_path='tests/tmp/3'")
+    run = os.system(
+        "python template/main.py --mode='UNKNOWN' --experiment_path='tests/tmp/3'"
+    )
     check.not_equal(run, 0)
 
     shutil.rmtree("tests/tmp")
