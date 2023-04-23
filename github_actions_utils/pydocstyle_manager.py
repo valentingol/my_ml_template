44af957f891aa3093a335b803e12f8f7b75788e5
Valentin Goldité <valentin.goldite@gmail.com>
2023-04-23 15:39:01 +0200

    🚀 Fix possible unbound variable

--- github_actions_utils/pydocstyle_manager.py
+++ github_actions_utils/pydocstyle_manager.py
@@ -11,10 +11,15 @@ def check_output() -> None:
         If Pydocstyle find errors.
     """
     args = sys.argv[1:]
+    n_errors = -1  # A default value
     for arg in args:
         if arg.startswith("--n_errors="):
             n_errors = int(arg.split("=")[1])
-
+    if n_errors == -1:
+        raise ValueError(
+            "Please specify the number of errors found by Pydocstyle "
+            "using the flag --n_errors=N."
+        )
     if n_errors > 0:
         raise ValueError(
             f"Pydocstyle found {n_errors} errors in python "
